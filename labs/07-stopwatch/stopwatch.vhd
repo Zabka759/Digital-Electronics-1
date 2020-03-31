e4835d234680d68c2932f3755abff1f2778aee89
Zabka759 <60696937+Zabka759@users.noreply.github.com>
2020-03-31 17:36:57 +0200

    07-stopwatches

---
+++ labs/07-stopwatch/stopwatch.vhd
@@ -0,0 +1,106 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    15:50:36 03/30/2020 
+-- Design Name: 
+-- Module Name:    stopwatch - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+use IEEE.NUMERIC_STD.ALL;
+use ieee.std_logic_unsigned.all;
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+library UNISIM;
+use UNISIM.VComponents.all;
+
+entity stopwatch is
+	port( 		--INPUTS--
+		clk_i 		: in std_logic; 	--clock
+		srst_n_i 	: in std_logic;  	--synchronous reset, active low
+		ce_100Hz_i 	: in std_logic; 	--clock_enable
+		cnt_en_i 	: in std_logic; 	--stopwatch enable by external switch
+		
+					--OUTPUTS--
+		sec_h_o 		: out std_logic_vector (4-1 downto 0);		--tens of seconds
+		sec_l_o 		: out std_logic_vector (4-1 downto 0);		--seconds
+		hth_h_o 		: out std_logic_vector (4-1 downto 0);		--tenths of seconds 
+		hth_l_o 		: out std_logic_vector (4-1 downto 0)		--hundreths of seconds 
+			);
+end stopwatch;
+
+architecture Behavioral of stopwatch is
+
+		signal s_hth_l : std_logic_vector(4-1 downto 0) := (others => '0');
+		signal s_hth_h : std_logic_vector(4-1 downto 0) := (others => '0');
+		signal s_sec_l : std_logic_vector(4-1 downto 0) := (others => '0');
+		signal s_sec_h : std_logic_vector(4-1 downto 0) := (others => '0');
+		
+begin
+		
+
+		stopwatch_cnt : process(clk_i)
+		begin
+				if rising_edge(clk_i) then
+					
+					if srst_n_i = '0' then 				--reset (clearing bits)
+						
+						s_hth_l <= (others => '0');	
+						s_hth_h <= (others => '0');
+						s_sec_l <= (others => '0');
+						s_sec_h <= (others => '0');
+					
+					elsif	ce_100Hz_i = '1' and cnt_en_i = '1' then	
+					
+							if s_sec_h = "0101" and s_sec_l = "1001" and s_hth_h = "1001" and s_hth_l = "1001" then --if 59.99 then clear all bits
+									
+									s_hth_l <= (others => '0');	
+									s_hth_h <= (others => '0');
+									s_sec_l <= (others => '0');
+									s_sec_h <= (others => '0');
+									
+							elsif s_sec_l = "1001" and s_hth_h = "1001" and s_hth_l = "1001" then --if X9.99 then clear 9.99 and inc X
+									
+									s_hth_l <= (others => '0');	
+									s_hth_h <= (others => '0');
+									s_sec_l <= (others => '0');
+									s_sec_h <= s_sec_h + 1;
+									
+							elsif s_hth_h = "1001" and s_hth_l = "1001" then --if YX.99 then clear .99 and increase X
+									
+									s_hth_l <= (others => '0');	
+									s_hth_h <= (others => '0');
+									s_sec_l <= s_sec_l + 1;
+							
+							elsif s_hth_l = "1001" then --if ZY.X9 then clear 9, inc X
+									
+									s_hth_l <= (others => '0');
+									s_hth_h <= s_hth_h + 1;
+							
+							else s_hth_l <= s_hth_l + 1; --if anything else, increase hundreths of seconds
+	
+							end if;
+						end if;
+					end if;
+					hth_l_o <= std_logic_vector(s_hth_l);
+					hth_h_o <= std_logic_vector(s_hth_h);
+					sec_l_o <= std_logic_vector(s_sec_l);
+					sec_h_o <= std_logic_vector(s_sec_h);
+			end process stopwatch_cnt;
+end architecture Behavioral;
+
---
+++ labs/07-stopwatch/top.vhd
@@ -0,0 +1,99 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    14:07:48 03/31/2020 
+-- Design Name: 
+-- Module Name:    top - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx primitives in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+entity top is
+port (
+    clk_i   	: in  std_logic;			-- Clock
+    BTN0  		: in std_logic;      	-- Synchronous reset srst_n_i
+	 BTN1  		: in std_logic;       	-- Stopwatch enable : '1' when not pushed so push to disable stopwatch counting
+    
+    disp_dp_o  : out std_logic;                       -- Decimal point
+    disp_seg_o : out std_logic_vector(7-1 downto 0);	--	Segments of 7seg disp
+    disp_dig_o : out std_logic_vector(4-1 downto 0)	-- Choice of one of four 7 seg displays
+);
+end top;
+
+----------------------------------------------------------------------------
+---Declaration of internal signals --
+-------------------------------------------------------------------------------
+
+architecture Behavioral of top is
+	 signal s_ce_100Hz_i  	: std_logic;
+	 signal s_data0  			: std_logic_vector(4-1 downto 0);
+	 signal s_data1  			: std_logic_vector(4-1 downto 0);
+	 signal s_data2  			: std_logic_vector(4-1 downto 0);
+	 signal s_data3  			: std_logic_vector(4-1 downto 0);
+begin
+
+-------------------------------------------------------------------------------
+---- SUB-BLOCKS ---
+-------------------------------------------------------------------------------
+
+	STOPWATCH : entity work.stopwatch
+			port map (
+				clk_i => clk_i,
+				srst_n_i => BTN0,				--synchronous reset
+				ce_100Hz_i => s_ce_100Hz_i,
+				cnt_en_i => BTN1,  
+				
+				sec_h_o => s_data3,
+				sec_l_o => s_data2,
+				hth_h_o => s_data1,
+				hth_l_o => s_data0
+			);
+
+	CLOCKEN : entity work.clock_enable
+			generic map (
+				g_NPERIOD => x"0064"       -- 10 ms 
+							)
+			port map (
+				clk_i          => clk_i,    
+				srst_n_i       => BTN0,     --synchronous reset
+				clock_enable_o => s_ce_100Hz_i
+		);
+		
+	DRIVER7SEG : entity work.driver_7seg
+	port map(
+				clk_i 	=> clk_i,  
+				srst_n_i => BTN0,
+				data0_i 	=> s_data0,   
+				data1_i 	=> s_data1,
+				data2_i 	=> s_data2, 
+				data3_i 	=> s_data3,
+				dp_i 		=> "1011",   
+    
+				dp_o 		=> disp_dp_o,                        
+				seg_o 	=> disp_seg_o,   
+				dig_o 	=> disp_dig_o   
+);
+	
+end Behavioral;
+
