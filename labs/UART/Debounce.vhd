cfbdea486e51e6dc3ba74cc66100eb35333fcfe4
Zabka759 <60696937+Zabka759@users.noreply.github.com>
2020-04-30 11:05:01 +0200

    Update Debounce.vhd

--- labs/UART/Debounce.vhd
+++ labs/UART/Debounce.vhd
@@ -3,11 +3,11 @@ use IEEE.STD_LOGIC_1164.ALL;
 
 entity Debounce is
 	port(	
-	btn_i  	: 	in 	std_logic;
-	f_samp_i : 	in		std_logic;
-	srst_n_i :  in    std_logic;
-	clk_i		:	in		std_logic;
-	deb_o 	:	out	std_logic);
+	btn_i  	 : in 	std_logic;
+	f_samp_i : in	std_logic;
+	srst_n_i : in   std_logic;
+	clk_i	 : in	std_logic;
+	deb_o 	 : out	std_logic);
 end Debounce;
 
 architecture Behavioral of Debounce is
@@ -19,12 +19,12 @@ begin
 			if(srst_n_i='0') then
 			sample_buffer <= "1111";													
 			elsif(f_samp_i='1') then													-- active every 2 ms (change in top.vhd SAMPLEBTN)
-				sample_buffer <= sample_buffer(2 downto 0) & btn_i;			-- filling register with log value of BTN1
+				sample_buffer <= sample_buffer(2 downto 0) & btn_i;	-- filling register with log value of BTN1
 			end if;
 		end if;
 	end process;
 	
-	deb_o <= '1' when sample_buffer="0000" else '0';							-- send '1' when button pressed long enough (autodisable when released)
+	deb_o <= '1' when sample_buffer="1000" else '0';			 	-- send '1' when button pressed long enough 
 	
 end Behavioral;
 
